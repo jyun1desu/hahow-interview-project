17123e9bd911bb59ed9a1aa8e8ca2b8842098343
jyun1desu <jyun1desu@25sprout.com>
2022-05-06 17:04:47 +0800

    feat: add RatioImage

---
+++ src/components/atoms/RatioImage/index.tsx
@@ -0,0 +1,38 @@
+import { styled } from "stitches.config";
+
+const RatioBox = styled("div", {
+  fontSize: 0,
+  position: "relative",
+  width: "100%",
+
+  "& > img": {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+    position: "absolute",
+    top: "0",
+    left: "0",
+  },
+});
+
+type ImageProps = {
+  path: string;
+  alt: string;
+  className?: string;
+  ratio: number;
+};
+
+const RatioImage = ({ className, path, alt, ratio }: ImageProps) => {
+  return (
+    <RatioBox
+      className={className}
+      style={{
+        paddingBottom: `${100 / ratio}%`,
+      }}
+    >
+      <img src={path} alt={alt} />
+    </RatioBox>
+  );
+};
+
+export default RatioImage;
---
+++ src/stitches.config.ts
@@ -0,0 +1,17 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  media: {
+    mb: "(min-width: 576px)",
+    tb: "(min-width: 996px)",
+  },
+});
